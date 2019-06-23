7b606b2c9a1ca822cd416e3741997ffed84fef22
Nompumelelo <lelonompumelelo@gmail.com>
2019-06-23 20:26:23 +0200

    Added number format package

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
+const NumberFormat = require('react-number-format');
 
-function App() {
+class  App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {cryptos: []};
+
+  }
+
+  componentWillMount() {
+    axios.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR&extraParams=your_app_name')
+      .then(res => {
+        const cryptos =res.data;
+        console.log(cryptos);
+        this.setState({crptos : cryptos})
+      })
+  }
+
+  componentDidMount() {
+      axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR&api_key=')
+        .then(res => {
+            const cryptos = res.data;
+            console.log(cryptos);
+            this.setState({cryptos: cryptos});
+        });
+  }
+
+  
+  
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <h4>Cryptocompare 2019</h4>
+
+       <div className="App">
+      {Object.keys(this.state.cryptos).map((key) => 
+          <div id="#crypto-holder">
+              <span className="left">{key}</span>
+              <span className="right"><NumberFormat={this.state.cryptos[key].USD} displayType={'text'} displayPrecision={2} /></span>
+          </div>
+        )}
+    </div> 
     </div>
+ 
   );
+  }
 }
 
 export default App;
